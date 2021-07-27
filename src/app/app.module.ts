4d8327c3bcc0db97daef88aee4a7afd96c924410
mingl1 <linm1@bxscience.edu>
2021-07-27 09:49:18 -0400

    5 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { CartService } from './cart.service';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'shipping', component: ShippingComponent },
+      { path: 'cart', component: CartComponent },
     ])
   ],
   declarations: [
@@ -23,11 +29,14 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
     TopBarComponent,
     ProductListComponent,
     ProductDetailsComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    CartComponent,
+    ShippingComponent
   ],
   bootstrap: [
     AppComponent
-  ]
+  ],
+  providers: [CartService]
 })
 export class AppModule { }
 
